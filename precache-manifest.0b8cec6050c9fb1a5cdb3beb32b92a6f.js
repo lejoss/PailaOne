d4183bf38f38f1d18b2adb1a435f6bf6d117d294
Alejandro Baez <nativo.alejandro@gmail.com>
2020-02-07 15:34:14 -0500

    Updates

---
+++ precache-manifest.0b8cec6050c9fb1a5cdb3beb32b92a6f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2a27cbec23c44fb886a04b8b87e466b",
+    "url": "/pailaone/index.html"
+  },
+  {
+    "revision": "0532537e8ba714519ab8",
+    "url": "/pailaone/static/css/main.a892d118.chunk.css"
+  },
+  {
+    "revision": "f762b09a8c43daebc9af",
+    "url": "/pailaone/static/js/2.668cad67.chunk.js"
+  },
+  {
+    "revision": "96f87ad404bc0babd707f401beaad320",
+    "url": "/pailaone/static/js/2.668cad67.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0532537e8ba714519ab8",
+    "url": "/pailaone/static/js/main.05878326.chunk.js"
+  },
+  {
+    "revision": "2ca38b70edf7598060e7",
+    "url": "/pailaone/static/js/runtime-main.ac0af2bf.js"
+  }
+]);
